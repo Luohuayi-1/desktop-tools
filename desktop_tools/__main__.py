e23bec289fc9063bacee1b42fa6dfada0da430ef
Luohuayi-1 <3224947697@qq.com>
2026-06-15 03:19:41 +0800

    chore: 预设系统暂时隐藏，后期优化再放开

--- desktop_tools/__main__.py
+++ desktop_tools/__main__.py
@@ -1,13 +1,6 @@
-"""Entry point: python -m desktop_tools
-   python -m desktop_tools annotator  # 启动标注台
-"""
+"""Entry point: python -m desktop_tools"""
 
-import sys
-
-if len(sys.argv) > 1 and sys.argv[1] == "annotator":
-    from .annotator import main
-else:
-    from .mcp_server import main
+from .mcp_server import main
 
 if __name__ == "__main__":
     main()
